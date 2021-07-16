8fecf8cf8747d4df443c5fee4b0c31f6e0ed31ce
Neil M <neilm813@gmail.com>
2021-07-16 09:10:08 -0700

    test case

--- algorithms/arrays/kMostFrequent.js
+++ algorithms/arrays/kMostFrequent.js
@@ -14,9 +14,9 @@ const k1 = 2;
 const expected1 = [1, 2];
 // Explanation: return the two most frequent elements, 1 and 2 are the two most frequent elements
 
-const nums2 = [0, 0, 0, 2, 2, 3];
+const nums2 = [0, 0, 2, 2, 2, 3];
 const k2 = 1;
-const expected2 = [0];
+const expected2 = [2];
 // Explanation: k being 1 means return the single most frequent element
 
 const nums3 = [1, 1, 2, 2, 3, 3];
