1b7c092194673b46495d9d488995f6c9e3978735
Neil M <neilm813@gmail.com>
2021-07-16 09:12:08 -0700

    slns

--- algorithms/arrays/twoSum.js
+++ algorithms/arrays/twoSum.js
@@ -16,11 +16,43 @@ const expected1 = [0, 2];
 
 /**
  * Finds the indexes of the nums that add up to the given target sum.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n) linear.
+ * - Space: O(n) linear.
  * @param {Array<number>} nums Unordered nums.
  * @param {number} targetSum
  * @returns {Array<number>} The two indexes of the numbers in the given nums
  *    that add up to the targetSum.
  */
-function twoSum(nums, targetSum) {}
+function twoSum(nums, targetSum) {
+  const numsAndIndices = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i],
+      diff = targetSum - num;
+
+    if (numsAndIndices.hasOwnProperty(diff)) {
+      const addendIdx = numsAndIndices[diff];
+      return [addendIdx, i];
+    }
+    numsAndIndices[num] = i;
+  }
+  return [];
+}
+
+/**
+ * - Time: O(n^2) quadratic.
+ * - Space: O(1) constant.
+ */
+function twoSumSpaceOptimized(nums, targetSum) {
+  const indexes = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; ++j) {
+      if (nums[i] + nums[j] === targetSum) {
+        indexes.push(i, j);
+        break;
+      }
+    }
+  }
+  return indexes;
+}
