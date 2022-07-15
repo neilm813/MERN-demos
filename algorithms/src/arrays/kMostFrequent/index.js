257f9d51f0a1f66084722274b1b77e339e3ed495
Neil M <neilm813@gmail.com>
2022-07-15 09:02:14 -0700

    algo reqs

---
+++ algorithms/src/arrays/kMostFrequent/index.js
@@ -0,0 +1,38 @@
+// From a technical interview with an AWS engineer: https://youtu.be/t0OQAD5gjd8
+
+/* 
+  Given an unsorted non-empty array of integers and int k, return the k most frequent elements (in any order)
+  You can assume there is always a valid solution
+  These example inputs are sorted for readability, but the input is NOT guaranteed to be sorted and the output does NOT need to be in any specific order
+  Hard Bonus: make it O(n) time
+*/
+
+const nums1 = [1, 1, 1, 2, 2, 3];
+const k1 = 2;
+const expected1 = [1, 2];
+// Explanation: return the two most frequent elements, 1 and 2 are the two most frequent elements
+
+const nums2 = [0, 0, 0, 2, 2, 3];
+const k2 = 1;
+const expected2 = [0];
+// Explanation: k being 1 means return the single most frequent element
+
+const nums3 = [1, 1, 2, 2, 3, 3];
+const k3 = 3;
+const expected3 = [1, 2, 3];
+/* 
+  Explanation: 3 is the only value that would be passed in for k because it is the only value for k that has
+  a valid solution. Since 1, 2, and 3, all occur 3 times, they are all the most frequent ints, so there is no
+  1 most frequent int, or 2 most frequent int, but there are 3 most frequent ints. 
+*/
+
+/**
+ * Returns the k most frequently occurring numbers from the given unordered
+ * nums.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums Unordered.
+ * @param {number} k Represents the amount of numbers that should be returned.
+ * @returns {Array<number>} The k most frequently occurring numbers.
+ */
+function kMostFrequent(nums, k) {}
