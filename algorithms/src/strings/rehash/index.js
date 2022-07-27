de637a49f13718893bcf0a7db59683c0c9f222ed
Neil M <neilm813@gmail.com>
2022-07-27 11:03:16 -0500

    algo reqs

---
+++ algorithms/src/strings/rehash/index.js
@@ -0,0 +1,16 @@
+/* 
+Given by Riot games.
+*/
+
+const str1 = "b70a164c32a20c10";
+const expected1 = "a184b70c42";
+
+/**
+ * Rehashes an incorrectly hashed string by combining letter count occurrences
+ * and alphabetizing them.
+ * Time: O(?).
+ * Space: O(?).
+ * @param {string} s An incorrectly hashed string.
+ * @returns {string} The correctly rehashed string alphabetized.
+ */
+function rehash(s) {}
