d4c8fca1e532fb7b05aae74235e6c55d29a977f7
Neil M <neilm813@gmail.com>
2022-09-06 15:47:24 -0500

    initial fetch and display

--- react-button-fetch-challenge/src/App.js
+++ react-button-fetch-challenge/src/App.js
@@ -1,6 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-const App = () => <div>test</div>;
+import './App.css';
+import { getErrorResponse, getUsers } from './services';
+import { User } from './components';
+
+const App = () => {
+  const [users, setUsers] = useState(null);
+  const [usersError, setUsersError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGetUsersClick = () => {
+    setIsLoading(true);
+
+    getUsers()
+      .then((data) => setUsers(data.users))
+      .catch((error) => setUsersError(error))
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <div>
+      <button onClick={handleGetUsersClick} type="button">
+        Get Users
+      </button>
+
+      {users && (
+        <section>
+          {users.map((user) => (
+            <User key={user.id} user={user} />
+          ))}
+        </section>
+      )}
+    </div>
+  );
+};
 
 export default App;
