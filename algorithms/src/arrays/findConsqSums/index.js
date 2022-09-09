67a0be5da8f96214f8683ca57e93dc8e200f84b4
Neil M <neilm813@gmail.com>
2022-09-09 10:44:48 -0500

    algo slns

--- algorithms/src/arrays/findConsqSums/index.js
+++ algorithms/src/arrays/findConsqSums/index.js
@@ -42,8 +42,8 @@ const expected5 = [
 
 /**
  * Finds all the sets of consecutive numbers that sum to the given target sum.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n^2 * (j-i)). Slice is at most `j - i` iterations.
+ * - Space: O(n^2 * (j-i)).
  * @param {Array<number>} nums Unordered nums.
  * @param {number} targetSum
  * @returns {Array<Array<number>>} 2d array where each nested array is a set of
@@ -51,4 +51,18 @@ const expected5 = [
  *    this context means the numbers whose indexes are one after the other
  *    only.
  */
-function findConsqSums(nums, targetSum) {}
+function findConsqSums(nums, targetSum) {
+  const summableSubsets = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = nums[i];
+
+    for (let j = i + 1; j <= nums.length; j++) {
+      if (sum === targetSum) {
+        summableSubsets.push(nums.slice(i, j));
+      }
+      sum += nums[j];
+    }
+  }
+  return summableSubsets;
+}
--- algorithms/src/arrays/twoSum/index.js
+++ algorithms/src/arrays/twoSum/index.js
@@ -23,11 +23,40 @@ const expected3 = [1, 6];
 
 /**
  * Finds the indexes of the nums that add up to the given target sum.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n) linear.
+ * - Space: O(n) linear.
  * @param {Array<number>} nums Unordered nums.
  * @param {number} targetSum
  * @returns {Array<number>} The two indexes of the numbers in the given nums
  *    that add up to the targetSum.
  */
-function twoSum(nums, targetSum) {}
+function twoSum(nums, targetSum) {
+  const numsToIndex = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const numA = nums[i];
+    const numB = targetSum - numA;
+
+    if (numsToIndex.hasOwnProperty(numB)) {
+      const idxB = numsToIndex[numB];
+      return [idxB, i];
+    }
+    numsToIndex[numA] = i;
+  }
+  return [];
+}
+
+/**
+ * - Time: O(n^2) quadratic.
+ * - Space: O(1) constant.
+ */
+function twoSumSpaceOptimized(nums, targetSum) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; ++j) {
+      if (nums[i] + nums[j] === targetSum) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
