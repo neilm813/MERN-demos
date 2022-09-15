fa6818f4b22528e7906d3d1b88b0013b31edb1d4
Neil M <neilm813@gmail.com>
2022-09-15 11:08:43 -0500

    algo slns

--- algorithms/src/objects/flattenObjectOfArrays/index.js
+++ algorithms/src/objects/flattenObjectOfArrays/index.js
@@ -42,9 +42,52 @@ const expected1 = [
 /**
  * Takes an object containing arrays of objects and places all the nested
  * objects into a new one-dim array.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n + (n * m)). n = num of keys on object, m = longest length of
+ *    nested array.
+ * - Space: O(n * m).
  * @param {Object} o Containing arrays of objects.
  * @returns {Object[]} An array of objects.
  */
-function flattenObjectOfArrays() {}
+function flattenObjectOfArrays(obj) {
+  const mergedArr = [];
+
+  for (const key in obj) {
+    const arrOfObjects = obj[key];
+
+    for (const nestedObj of arrOfObjects) {
+      // Extract only the keys wanted.
+      const data = {
+        id: nestedObj.id,
+        type: nestedObj.type,
+      };
+      mergedArr.push(data);
+    }
+  }
+
+  return mergedArr;
+}
+
+/**
+ * Takes an object containing arrays of objects and places all the nested
+ * objects into a new one-dim array.
+ * - Time: O(n + (n * m) + (n * m)) -> O(n + 2(n * m)) -> O(n + (n * m)).
+ *    n = num of keys on object, m = longest length of.
+ *    The first n is from Object.values looping over all keys to make an array.
+ *    Then n * m b/c flat loops over a 2d array to flatten it,
+ *    then n * m again b/c .map has to loop over the n * m amount of items that
+ *    are now in a flattened array.
+ *    nested array.
+ * - Space: O(n * m).
+ * @param {Object} o Containing arrays of objects.
+ * @returns {Object[]} An array of objects.
+ */
+const functionalFlattenObjectOfArrays = (o) =>
+  // Get an array of the object's values. Since they are arrays it will be 2d.
+  Object.values(o)
+    // Flatten 2d to 1d array.
+    .flat()
+    // Transform the data to the structure we want (not all keys are wanted).
+    .map(({ id, type }) => ({
+      id,
+      type,
+    }));
