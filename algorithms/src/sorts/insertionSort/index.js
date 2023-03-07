19af31a0bc77a1406b3122e00b8060cbe496b536
Neil M <neilm813@gmail.com>
2023-03-07 10:09:19 -0600

    spelling

--- algorithms/src/sorts/insertionSort/index.js
+++ algorithms/src/sorts/insertionSort/index.js
@@ -3,15 +3,20 @@
       https://www.khanacademy.org/computing/computer-science/algorithms/insertion-sort/a/insertion-sort
   - Array / bar visualization:
       https://www.hackerearth.com/practice/algorithms/sorting/insertion-sort/visualize/
+
   - Stable sort, efficient for small data sets or mostly sorted data sets.
+
   Time Complexity
     - Best: O(n) linear when array is already sorted.
     - Average: O(n^2) quadratic.
     - Worst: O(n^2) quadratic when the array is reverse sorted.
+
   Space: O(1) constant.
+
   - this sort splits the array into two portions (conceptually, not literally).
   - the left portion will become sorted, the right portion
       (that hasn't been iterated over yet) is unsorted.
+
   can shift OR swap target element until it reaches desired position
   shifting steps:
   1. consider the first item as sorted
@@ -22,6 +27,7 @@
   5. repeat step 4 as many times as needed
   6. insert current item
   7. move to the next item and repeat
+
   swap steps:
   1. consider the first item as sorted
   2. move to the next item
@@ -31,8 +37,8 @@
 */
 
 const numbersOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const numbersRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
-const numbersReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const numberRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numberReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 /**
@@ -40,7 +46,7 @@ const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
  * Best: O(n) linear when array is already sorted.
  * Average: O(n^2) quadratic.
  * Worst: O(n^2) quadratic when the array is reverse sorted.
- * @param {Array<number>} numbers
+ * @param {Array<number>} number
  * @returns {Array<number>} The given array after being sorted.
  */
-function insertionSort(numbers = []) {}
+function insertionSort(number = []) {}
--- algorithms/src/sorts/selectionSort/index.js
+++ algorithms/src/sorts/selectionSort/index.js
@@ -6,13 +6,16 @@
   position, which is now sorted, iterate through the list again to find the
   next minimum value and move it to index 1. This continues until all values
   are sorted.
+
   Unstable sort.
   
   Time Complexity
     - Best: O(n^2) quadratic.
     - Average: O(n^2) quadratic.
     - Worst: O(n^2) quadratic.
+
   Space: O(1) constant.
+
   Selection sort is one of the slower sorts.
   - ideal for: pagination, where page 1 displays 10 records for example,
       you run selection sort for 10 iterations only to display the first 10
