6248eed0744255364fe8fdc027fe13ab4306733a
Neil M <neilm813@gmail.com>
2023-03-17 11:13:34 -0500

    algos

--- algorithms/src/arrays/twoSum/index.js
+++ algorithms/src/arrays/twoSum/index.js
@@ -21,11 +21,69 @@ const expected3 = [1, 6];
 
 /**
  * Finds the indexes of the numbers that add up to the given target sum.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n) linear.
+ * - Space: O(n) linear.
  * @param {Array<number>} numbers Unordered numbers.
  * @param {number} targetSum
  * @returns {Array<number>} The two indexes of the numbers in the given numbers
  *    that add up to the targetSum.
  */
-function twoSum(numbers, targetSum) {}
+function twoSum(numbers, targetSum) {
+  const numbersToIndex = {};
+
+  for (let i = 0; i < numbers.length; i++) {
+    const numA = numbers[i];
+    const numB = targetSum - numA;
+
+    if (numbersToIndex.hasOwnProperty(numB)) {
+      const idxB = numbersToIndex[numB];
+      return [idxB, i];
+    }
+    numbersToIndex[numA] = i;
+  }
+  return [];
+}
+
+/**
+ * NOTE: The `Map` object is often preferred over the plain object when
+ * dynamically adding keys and in many other circumstances that you can
+ * read more about.
+ *
+ * Finds the indexes of the numbers that add up to the given target sum.
+ * - Time: O(n) linear.
+ * - Space: O(n) linear.
+ * @param {Array<number>} numbers Unordered numbers.
+ * @param {number} targetSum
+ * @returns {Array<number>} The two indexes of the numbers in the given numbers
+ *    that add up to the targetSum.
+ */
+function twoSumMap(numbers, targetSum) {
+  const numbersToIndex = new Map();
+
+  for (let i = 0; i < numbers.length; i++) {
+    const numA = numbers[i];
+    const numB = targetSum - numA;
+
+    if (numbersToIndex.has(numB)) {
+      const idxB = numbersToIndex.get(numB);
+      return [idxB, i];
+    }
+    numbersToIndex.set(numA, i);
+  }
+  return [];
+}
+
+/**
+ * - Time: O(n^2) quadratic.
+ * - Space: O(1) constant.
+ */
+function twoSumSpaceOptimized(numbers, targetSum) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; ++j) {
+      if (numbers[i] + numbers[j] === targetSum) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
