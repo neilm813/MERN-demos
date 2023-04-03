d6732e32d61d86371df89e75d550c067c2f6467d
Neil M <neilm813@gmail.com>
2023-04-03 10:57:21 -0500

    algos

--- algorithms/src/sorts/bubbleSort/index.js
+++ algorithms/src/sorts/bubbleSort/index.js
@@ -28,4 +28,74 @@ const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
  * @param {Array<number>} numbers
  * @returns {Array<number>} The given numbers after being sorted.
  */
-function bubbleSort(numbers = []) {}
+function bubbleSort(numbers = []) {
+  let isSorted = false;
+
+  while (isSorted === false) {
+    isSorted = true;
+
+    for (let i = 0; i < numbers.length - 1; i++) {
+      const j = i + 1;
+
+      if (numbers[i] > numbers[j]) {
+        isSorted = false;
+        const temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+      }
+    }
+  }
+  return numbers;
+}
+
+/**
+ * A comparison function to determines how to order based on the return value.
+ *    - `=== 0`: keep original order of `a` and `b`
+ *    - `< 0`: sort `a` before `b`
+ *    - `> 0`: sort `a` after `b`
+ * @callback CompareFn
+ * @param {any} a The left item.
+ * @param {any} b The right item.
+ * @returns {number} Determines if `a` and `b` are swapped or not.
+ */
+
+/**
+ * Sorts the given items in-place by mutating the array.
+ * Best: O(n) linear when array is already sorted.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic when the array is reverse sorted.
+ * @param {Array<any>} items
+ * @param {CompareFn} compareFn
+ * @example
+ * ```
+ * // By default this is sorted in ascending order.
+ * bubbleSortFlexible([5, 3, 6, 1, 8]);
+ *
+ * // Descending sort.
+ * bubbleSortFlexible([5, 3, 6, 1, 8], (a, b) => b - a);
+ *
+ * // Sort by person.age ascending.
+ * bubbleSortFlexible(persons, (a, b) => a.age - b.age);
+ * ```
+ * @returns {Array<number>} The given items after being sorted.
+ */
+function bubbleSortFlexible(items = [], compareFn = (a, b) => a - b) {
+  let isSorted = false;
+
+  while (isSorted === false) {
+    isSorted = true;
+
+    for (let idxA = 0; idxA < items.length - 1; idxA++) {
+      const idxB = idxA + 1;
+      const comparisonResult = compareFn(items[idxA], items[idxB]);
+
+      if (comparisonResult > 0) {
+        isSorted = false;
+        const temp = items[idxA];
+        items[idxA] = items[idxB];
+        items[idxB] = temp;
+      }
+    }
+  }
+  return items;
+}
