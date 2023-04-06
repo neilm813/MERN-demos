459f44c82a3831d14ecd383910065269192e533c
Neil M <neilm813@gmail.com>
2023-04-06 12:32:11 -0500

    new counter form

--- count-all-the-things/src/App.js
+++ count-all-the-things/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+import { useState } from 'react';
+
 import Counter from './components/Counter';
 
 function App() {
-  const counterDataFromFakeDb = [
+  const [title, setTitle] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [startCount, setStartCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const [titleValidationError, setTitleValidationError] = useState('Required field.');
+  const [imageUrlValidationError, setImageUrlValidationError] = useState('Required field.');
+
+  const [counters, setCounters] = useState([
     {
       title: 'Studious Students',
       imageUrl: 'https://media.tenor.com/8yQGBBHCHlcAAAAM/boy-math.gif',
@@ -32,7 +42,58 @@ function App() {
       imageUrl: 'https://study.com/cimages/multimages/16/180_angle2483887147979442891.png',
       step: 180,
     },
-  ];
+  ]);
+
+  const handleSubmitNewCounter = (event) => {
+    // Prevent the default form submit page refresh behavior.
+    event.preventDefault();
+
+    const newCounter = {
+      // Long-form - keyName: value
+      title: title,
+      // Shorthand - can be used when the key name matches the name of the var used for the value
+      imageUrl,
+      startCount,
+      step,
+    };
+
+    // Create a new array with the newCounter at the front then copy all the other counter objects after it.
+    // When your state is an object or an array, the set state function won't re-render unless you pass in a NEW object or
+    // array. DO NOT MUTATE state directly, copy -> update -> set state.
+    const updatedCounters = [newCounter, ...counters];
+    setCounters(updatedCounters);
+
+    console.log(newCounter);
+    // Reset the form state, this REQUIRES adding the value attribute to the <input />
+    setTitle('');
+    setImageUrl('');
+    setStartCount(0);
+    setStep(1);
+  };
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+
+    if (event.target.value.length < 2) {
+      setTitleValidationError('Minimum length is 2.');
+    } else {
+      setTitleValidationError('');
+    }
+  };
+
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
+
+    const regex = new RegExp(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi);
+
+    if (event.target.value.match(regex)) {
+      setImageUrlValidationError('');
+    } else {
+      setImageUrlValidationError('Invalid format.');
+    }
+  };
+
+  const areAnyValidationErrors = Boolean(titleValidationError || imageUrlValidationError);
 
   return (
     <div className="min-h-screen text-slate-700 dark:text-slate-200 bg-white dark:bg-slate-900">
@@ -42,11 +103,95 @@ function App() {
           <small>Not everything that can be counted matters; not everything that matters can be counted.</small>
         </p>
       </header>
+
+      <form
+        onSubmit={(event) => {
+          handleSubmitNewCounter(event);
+        }}
+        className="w-1/5 mx-auto mt-9 p-8 rounded border border-gray-200 dark:border-gray-700"
+      >
+        <h3 className="text-2xl font-bold text-center mb-6">Add A Counter</h3>
+
+        <div className="mb-6">
+          <label htmlFor="new-counter-title" className="block mb-2 text-lg font-medium">
+            Title
+          </label>
+          <input
+            onChange={(event) => {
+              handleTitleChange(event);
+            }}
+            value={title}
+            type="text"
+            id="new-counter-title"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          {titleValidationError && (
+            <p className="mt-2 text-sm text-red-600 dark:text-red-600">{titleValidationError}</p>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="new-counter-imageUrl" className="block mb-2 text-lg font-medium">
+            Image Url
+          </label>
+          <input
+            onChange={(event) => {
+              handleImageUrlChange(event);
+            }}
+            value={imageUrl}
+            type="text"
+            id="new-counter-imageUrl"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          {imageUrlValidationError && (
+            <p className="mt-2 text-sm text-red-600 dark:text-red-600">{imageUrlValidationError}</p>
+          )}
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="new-counter-startCount" className="block mb-2 text-lg font-medium">
+            Start Count
+          </label>
+          <input
+            onChange={(event) => {
+              setStartCount(event.target.value);
+            }}
+            value={startCount}
+            type="number"
+            id="new-counter-startCount"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="new-counter-step" className="block mb-2 text-lg font-medium">
+            Step
+          </label>
+          <input
+            onChange={(event) => {
+              setStep(event.target.value);
+            }}
+            value={step}
+            type="number"
+            id="new-counter-step"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+
+        <button
+          disabled={areAnyValidationErrors}
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+
       <hr className="h-px my-8 bg-gray-200 dark:bg-gray-700 border-0 " />
 
       <div className="container mx-auto">
         <main className="flex flex-col justify-items-center items-center">
-          {counterDataFromFakeDb.map((data, i) => {
+          {counters.map((data, i) => {
             return <Counter key={`${i}-${data.title}`} counterData={data} />;
           })}
         </main>
