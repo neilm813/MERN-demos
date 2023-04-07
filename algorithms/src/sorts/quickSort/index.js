800c07dc2ee51b73860c0d939a4ed772ecef96a5
Neil M <neilm813@gmail.com>
2023-04-07 10:05:25 -0500

    algos

--- algorithms/src/sorts/quickSort/index.js
+++ algorithms/src/sorts/quickSort/index.js
@@ -1,3 +1,43 @@
+/* 
+  Visualization:
+  https://www.hackerearth.com/practice/algorithms/sorting/quick-sort/visualize/
+  Create a function that uses yesterday’s partition to fully sort an array
+  in-place.
+  Unstable sort.
+  
+  Time Complexity
+    - Best: O(n log(n)) linearithmic.
+    - Average: O(n log(n)) linearithmic.
+    - Worst: O(n^2) quadratic.
+  
+  Space: O(1) constant.
+  Params: nums, left, right
+  - left and right are indexes, for now, left will be 0, and right will be the
+      last idx.
+  - later these params will be used to specify a sub section of the array to
+      partition.
+  Steps:
+    - start by partitioning the full array
+        (use the previously built partition algo).
+    - then partition the left side of the array
+        (left of the returned partition idx) and the right side
+        (right of the returned partition idx), recursively.
+*/
+
+const { partition } = require('../partition');
+
+const numbers1 = [11, 8, 14, 3, 6, 2, 7];
+const expected1 = [2, 3, 6, 7, 8, 11, 14];
+
+const numbers2 = [1, 17, 12, 3, 9, 13, 21, 4, 27];
+const expected2 = [1, 3, 4, 9, 12, 13, 17, 21, 27];
+
+const numbers3 = [11, 8, 14, 3, 3, 3, 6, 2, 7];
+const expected3 = [2, 3, 3, 3, 6, 7, 8, 11, 14];
+
+const numbers4 = [1, 17, 12, 3, 9, 13, 21, 4, 27];
+const expected4 = [1, 3, 4, 9, 12, 13, 17, 21, 27];
+
 /**
  * Recursively sorts the given array in-place by mutating the array.
  * Best: O(n log(n)) linearithmic.
