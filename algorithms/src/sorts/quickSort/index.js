ab7e0e3b8e1c95011e2e20055a4b2c855090c4fd
Neil M <neilm813@gmail.com>
2023-04-07 10:04:52 -0500

    algos

---
+++ algorithms/src/sorts/quickSort/index.js
@@ -0,0 +1,15 @@
+/**
+ * Recursively sorts the given array in-place by mutating the array.
+ * Best: O(n log(n)) linearithmic.
+ * Average: O(n log(n)) linearithmic.
+ * Worst: O(n^2) quadratic.
+ * @see https://www.hackerearth.com/practice/algorithms/sorting/quick-sort/visualize/
+ *    visualization.
+ * @param {Array<number>} numbers
+ * @param {number} left The index indicating the start of the slice of the
+ *    given array being processed.
+ * @param {number} right The index indicating the end of the slice of the
+ *    given array being processed.
+ * @returns {Array<number>} The given array after being sorted.
+ */
+function quickSort(numbers = [], left = 0, right = numbers.length - 1) {}
