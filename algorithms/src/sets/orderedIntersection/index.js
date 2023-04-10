8392eae2ee33c9418deb14aafe6231d791b22f14
Neil M <neilm813@gmail.com>
2023-04-10 10:57:12 -0500

    algos

--- algorithms/src/sets/orderedIntersection/index.js
+++ algorithms/src/sets/orderedIntersection/index.js
@@ -23,11 +23,33 @@ const expected3 = [];
  * Efficiently combine the two sorted arrays into a new array that is the a sorted set intersection.
  * Venn Diagram Visualization (bottom):
  * @see https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n) linear, n = max(sortedA.length, sortedB.length) when there are dupes we may end up looping over all
+ *    items of longer arr.
+ * - Space: O(n) linear, n = shorter array length.
  * @param {Array<number>} sortedA
  * @param {Array<number>} sortedB Both sets are multi-sets (multi in that it can contain multiple dupes).
  * @returns {Array<number>} The sorted set intersection: a new array that is sorted and contains only the shared values
  *    between the two arrays de-duped.
  */
-function orderedIntersection(sortedA, sortedB) {}
+function orderedIntersection(sortedA, sortedB) {
+  let idxA = 0;
+  let idxB = 0;
+
+  const intersection = [];
+
+  while (idxA < sortedA.length && idxB < sortedB.length) {
+    if (sortedA[idxA] === sortedB[idxB]) {
+      if (intersection[intersection.length - 1] !== sortedA[idxA]) {
+        // add it only if the last num added isn't the same num
+        intersection.push(sortedA[idxA]);
+      }
+      idxA++;
+      idxB++;
+    } else if (sortedA[idxA] < sortedB[idxB]) {
+      idxA++;
+    } else {
+      idxB++;
+    }
+  }
+  return intersection;
+}
