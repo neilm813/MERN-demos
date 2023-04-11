f04ce06c1c1443ef0a8a8a9bc4e8dba97d7913f9
Neil M <neilm813@gmail.com>
2023-04-11 11:06:34 -0500

    algos

--- algorithms/src/sets/orderedMultisetUnion/index.js
+++ algorithms/src/sets/orderedMultisetUnion/index.js
@@ -38,8 +38,12 @@ const expected5 = [];
  * Combines two already sorted multiset arrays into an ordered multiset union
  * Venn Diagram Visualization (top):
  * @see https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n + m) linear, n = sortedA.length, m = sortedB.length because
+ *  we may be pushing from only 1 array at a time while the other array's idx
+ *  is staying in place. At worst, we push all items from 1 array when that
+ *  array has all smaller items and then iterate through the 2nd array after.
+ * - Space: O(n + m) where n = sortedA.length, m = sortedB.length because if
+ *    there are no dupes all will be kept from both.
  * @param {Array<number>} sortedA Both sets are sorted multisets
  *    (contain dupes).
  * @param {Array<number>} sortedB
@@ -48,4 +52,56 @@ const expected5 = [];
  *    should be based on the max amount that dupe appears from one set,
  *    not the combined amount from both sets.
  */
-function orderedMultisetUnion(sortedA, sortedB) {}
+function orderedMultisetUnion(sortedA, sortedB) {
+  const union = [];
+  let idxA = 0;
+  let idxB = 0;
+
+  while (idxA < sortedA.length || idxB < sortedB.length) {
+    if (idxA === sortedA.length) {
+      // sortedB is longer, push in all remaining sortedB numbers
+      union.push(sortedB[idxB++]);
+      continue;
+    } else if (idxB === sortedB.length) {
+      // sortedA is longer, push in remaining sortedA numbers
+      union.push(sortedA[idxA++]);
+      continue;
+    }
+
+    if (sortedA[idxA] === sortedB[idxB]) {
+      union.push(sortedA[idxA++]);
+      idxB++; // since both were same, increment both
+    } else if (sortedA[idxA] < sortedB[idxB]) {
+      union.push(sortedA[idxA++]);
+    } else {
+      union.push(sortedB[idxB++]);
+    }
+  }
+  return union;
+}
+
+function orderedMultisetUnion2(sortedA, sortedB) {
+  let idxA = 0;
+  let idxB = 0;
+
+  const union = [];
+
+  while (idxA < sortedA.length && idxB < sortedB.length) {
+    const numberA = sortedA[idxA];
+    const numberB = sortedB[idxB];
+
+    if (numberA === numberB) {
+      union.push(numberA);
+      idxA++;
+      idxB++;
+    } else if (numberA < numberB) {
+      union.push(numberA);
+      idxA++;
+    } else {
+      union.push(numberB);
+      idxB++;
+    }
+  }
+  // arrays might be different lengths, if any elements are remaining, concat them
+  return union.concat(sortedA.slice(idxA)).concat(sortedB.slice(idxB));
+}
