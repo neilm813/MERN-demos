31adf4a15c4b42accf6831b941a8f2bb08881038
Neil M <neilm813@gmail.com>
2023-04-18 10:13:02 -0500

    algos

---
+++ algorithms/src/objects/findObjects/index.js
@@ -0,0 +1,50 @@
+/* 
+  Given a search criteria object whose values will only be
+  primitives (ints, strings, booleans) and a list of objects.
+
+  return any object that matches all the key value pairs in the search
+  criteria object.
+
+  Bonus: write a 2nd solution using build in methods to practice functional
+  programming.
+*/
+
+const items = [
+  { firstName: 'Bob', lastName: 'Bobbert', age: 31 },
+  { firstName: 'John', lastName: 'Smith', age: 25 },
+  { firstName: 'Bob', lastName: 'Smith', age: 27 },
+  { firstName: 'Bob', lastName: 'White', age: 31 },
+];
+
+const searchCriteria1 = {
+  firstName: 'Bob',
+  age: 31,
+};
+const expected1 = [
+  { firstName: 'Bob', lastName: 'Bobbert', age: 31 },
+  { firstName: 'Bob', lastName: 'White', age: 31 },
+];
+
+const searchCriteria2 = {
+  lastName: 'Smith',
+};
+const expected2 = [
+  { firstName: 'John', lastName: 'Smith', age: 25 },
+  { firstName: 'Bob', lastName: 'Smith', age: 27 },
+];
+
+/**
+ * Finds the objects that match the given search criteria.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Object} criteria
+ * @param {Array<Object>} collection
+ * @returns {Array<Object>} The found objects.
+ */
+function findObjects(criteria, collection) {}
+
+/**
+ * - Time: O(?).
+ * - Space: O(?).
+ */
+function findObjectsFunctional(criteria, collection) {}
