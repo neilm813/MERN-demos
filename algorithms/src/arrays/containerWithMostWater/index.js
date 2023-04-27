3ed5b0793afbb8531974601510d4e433c848b5df
Neil M <neilm813@gmail.com>
2023-04-27 10:02:47 -0500

    algos

---
+++ algorithms/src/arrays/containerWithMostWater/index.js
@@ -0,0 +1,37 @@
+/* 
+https://leetcode.com/problems/container-with-most-water/
+*/
+
+/* 
+Finds the container that can hold the most water based on it's area.
+A container's length is the distance between indexes and the two sides are
+the heights at those indexes.
+
+See: https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg
+*/
+
+const heights1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+const expected1 = 49;
+// Explanation: heights1[1] and heights1[8] as container edges.
+// Length = 8 - 1. Height = 7
+
+const heights2 = [1, 1];
+const expected2 = 1;
+
+const heights3 = [4, 3, 2, 1, 4];
+const expected3 = 16;
+
+const heights4 = [1, 2, 1];
+const expected4 = 2;
+
+/**
+ * Finds the container that can hold the most water based on it's area.
+ * A container's length is the distance between indexes and the two sides are
+ * the heights at those indexes.
+ * @see https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number[]} heights
+ * @returns {number} Representing the max area of a container.
+ */
+function containerWithMostWater(heights) {}
