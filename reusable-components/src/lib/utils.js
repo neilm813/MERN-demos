c945df1fd514e20d8b6bc0b6e8fbdf1a204e17f7
Neil M <neilm813@gmail.com>
2023-11-16 11:44:38 -0600

    setup at import alias

---
+++ reusable-components/src/lib/utils.js
@@ -0,0 +1,8 @@
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const cn = (...inputs) => {
+  return twMerge(clsx(inputs));
+};
+
+export const utilTest = () => console.log('util test');
