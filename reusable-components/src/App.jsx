4e4b547cf19c5f19f445a2ef6b8260c7ed8bba1a
Neil M <neilm813@gmail.com>
2023-11-16 12:24:44 -0600

    finish setting tailwind to use with shad cn

--- reusable-components/src/App.jsx
+++ reusable-components/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import './App.css';
 
+import { Button } from '@/components';
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="w-5/6 mx-auto">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Default</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="outline">Outline</Button>
       <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-    </>
+    </div>
   );
 }
 
